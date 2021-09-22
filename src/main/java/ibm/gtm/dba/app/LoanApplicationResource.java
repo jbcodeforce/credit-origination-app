0bacf2e8336c958e185a4b20b3999e5da9b5653e
Jerome Boyer <jerome.boyer@gmail.com>
2021-09-21 18:05:19 -0700

    add loan application entity

--- src/main/java/ibm/gtm/dba/app/LoanApplicationResource.java
+++ src/main/java/ibm/gtm/dba/app/LoanApplicationResource.java
@@ -1,14 +1,18 @@
 package ibm.gtm.dba.app;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-@Path("/hello")
-public class GreetingResource {
+import ibm.gtm.dba.domain.LoanApplication;
+
+@Path("/api/v1/loans")
+public class LoanApplicationResource {
 
     @ConfigProperty(name="app.version")
     public String version;
@@ -21,8 +25,15 @@ public class GreetingResource {
 
     @GET
     @Path("/version")
-    @Produces()
+    @Produces(MediaType.APPLICATION_JSON)
     public String getVersion(){
         return "{ \"version\": \"" + version + "\"}";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String createLoanApplication(LoanApplication loanApplication){
+        System.out.println(loanApplication.toString());
+        return "{ \"loanApplicationId\" : 123 \"}";
     }
 }
---
+++ src/main/java/ibm/gtm/dba/domain/LoanApplication.java
@@ -0,0 +1,10 @@
+package ibm.gtm.dba.domain;
+
+public class LoanApplication {
+    public String loanApplicationId;
+    public String primaryApplicantName;
+    public String secondaryApplicantName;
+    public double loanAmount;
+    public String propertyType;
+    public String loanPurposeType;
+}
